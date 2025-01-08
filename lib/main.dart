e763db829e91858c1c56976e12c838950e0e052e
miriam santi <miriam.santi.stud@ispascalcomandini.it>
2025-01-08 12:57:00 +0100

    chat che salva i messss

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,21 @@
 import 'dart:convert';
-import 'dart:io';
+import 'dart:async';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:flutter_chat_types/flutter_chat_types.dart' as types;
 import 'package:flutter_chat_ui/flutter_chat_ui.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:flutter/services.dart' show rootBundle;
 import 'package:uuid/uuid.dart';
+import 'dart:io';
 import 'package:path_provider/path_provider.dart';
-import 'package:http/http.dart' as http;
-import 'package:socket_io_client/socket_io_client.dart' as IO;
-import 'package:quickalert/quickalert.dart';
-
+import 'package:socket_io_client/socket_io_client.dart' as io;  //messo minuscolo per risolvere un problema che non volevo avere
 void main() {
   initializeDateFormatting().then(
     (_) => runApp(
       const SimpleChat(),
     ),
+
   );
 }
 
@@ -25,16 +25,15 @@ class SimpleChat extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Test',
+      title: "Flutter Test",
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Chat'),
+      home: const MyHomePage(title: "La grande Chat")
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
@@ -43,78 +42,51 @@ class MyHomePage extends StatefulWidget {
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   List<types.Message> _messaggi = [];
-  final _user = const types.User(
-    id: '82091008-a484-4a89-ae75-a22bf8d6f3ac',
-  );
-  late IO.Socket socket;
-  String errorMessage = '';
-  List<dynamic> jsonArray = [];
+  final _user = const types.User(id: '82ejf58-a484-4a89-ae75-a22bf8d6f3acertgertert',);
+  late io.Socket socket;
+  final StreamController<String> _stControl = StreamController<String>();
+  Stream<String> get msgStream => _stControl.stream;
+  String toSend = "no";
 
-  @override
-  void dispose() {
-    socket.disconnect();
-    super.dispose();
-  }
-
+  
   @override
   void initState() {
     super.initState();
-    _loadMessages();
-
-    socket = IO.io("http://10.1.0.5:3000", <String, dynamic>{
-      'transports': ['websocket']
-    });
-
-    socket.on('connect', (_) {
-      setState(() {
-        errorMessage = 'connesso al server';
-      });
+    socket = io.io("http://10.1.0.6:3000", <String, dynamic>{"transports": ["websocket"]});
+    socket.on("connect", (_) {
     });
 
     socket.on('message', (data) {
-       final messageData = jsonDecode(data);
-    final receivedMessage = types.TextMessage.fromJson(messageData);
+      final messageData = jsonDecode(data);
+      final receivedMessage = types.TextMessage.fromJson(messageData);
 
-     final otherUser = const types.User(
-       id: 'other-user-id', 
-     );
+      final otherUser = const types.User(
+        id: 'other-user-id', 
+      );
 
-     final newMessage = receivedMessage.copyWith(author: otherUser);
-
-    setState(() {
-      _messaggi.insert(0, newMessage);
-    });
+      final newMessage = receivedMessage.copyWith(author: otherUser);
+      addMessage(newMessage);
+      // setState(() {
+      //   _messaggi.insert(0, newMessage);
+        
+      // });
   
     });
+    _loadMessages();
   }
 
-  void _loadMessages() async {
-    //   final response = await rootBundle.loadString('assets/messaggi.json');
-    //   final messages = (jsonDecode(response) as List).map((e) => types.Message.fromJson(e as Map<String, dynamic>)).toList();
-    //  setState(() {
-    //     _messaggi = messages;
-    //   });
-    final filepath = await getFilePath();
-    final file = File(filepath);
-    if (await file.exists()) {
-      final jsonString = await file.readAsString();
-      if (jsonString.isNotEmpty) {
-        final jsonList = (jsonDecode(jsonString) as List)
-            .map((e) => types.Message.fromJson(e as Map<String, dynamic>))
-            .toList();
-        setState(() {
-          _messaggi = jsonList;
-        });
-      }
-    }
+  @override
+  void dispose(){
+    socket.disconnect();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+
+
       body: Chat(
         messages: _messaggi,
         onPreviewDataFetched: _handlePreviewDataFetched,
@@ -123,60 +95,83 @@ class _MyHomePageState extends State<MyHomePage> {
         showUserNames: true,
         user: _user,
         theme: const DefaultChatTheme(
-            seenIcon: Text(
-          'read',
-          style: TextStyle(
-            fontSize: 10.0,
-          ),
-        )),
+          seenIcon: Text("read", style: TextStyle(fontSize: 10.0),)
+        ),
       ),
     );
   }
+  Future<void> _checkExistence() async{
+    final file = File(await _getFilePath());
+    if(await file.exists()){
+    }else{
+      final response = await rootBundle.loadString("assets/messaggi.json");
+      final messages = (jsonDecode(response) as List).map((e) => types.Message.fromJson(e as Map<String, dynamic>)).toList();
+      _messaggi = messages;
+      final jsonString = jsonEncode(_messaggi);
+      await file.writeAsString(jsonString);
 
-  void _handlePreviewDataFetched(
-    types.TextMessage message,
-    types.PreviewData previewData,
-  ) {
+    }
+    return;
+  }
+
+  void _loadMessages() async{
+    await _checkExistence();
+    final file = File(await _getFilePath());
+    final messages = (jsonDecode(await file.readAsString()) as List).map((e) => types.Message.fromJson(e as Map<String, dynamic>)).toList();
+  
+  setState(() {_messaggi = messages;});
+  }
+  void _handlePreviewDataFetched( types.TextMessage message, types.PreviewData previewData){
     final index = _messaggi.indexWhere((element) => element.id == message.id);
-    final updatedMessage = (_messaggi[index] as types.TextMessage).copyWith(
-      previewData: previewData,
-    );
-
+    final updatedMessage = (_messaggi[index] as types.TextMessage).copyWith( previewData: previewData);
+    sendMessage("a");
     setState(() {
       _messaggi[index] = updatedMessage;
     });
   }
-
-  void _handleSendPressed(types.PartialText message) async {
-    final textMessage = types.TextMessage(
-        author: _user,
-        createdAt: DateTime.now().millisecondsSinceEpoch,
-        id: const Uuid().v4(),
-        text: message.text);
-
-    _addMessage(textMessage);
+  void _handleSendPressed(types.PartialText message) async{
+    final textMessage = types.TextMessage(author: _user, id: const Uuid().v4(), text: message.text, createdAt: DateTime.now().millisecondsSinceEpoch);
+    await prepMsg(textMessage);
+    sendMessage(toSend);
+    setState(() {
+      _loadMessages();
+    });
   }
 
-  void _addMessage(types.Message message) async {
-    final filepath = await getFilePath();
-    final file = File(filepath);
+  Future<String> _getFilePath() async {
+    final directory = await getApplicationDocumentsDirectory();
+    return "${directory.path}/messaggi.json";
+  }
 
-    setState(() {
-      _messaggi.insert(0, message);
-    });
-    final jsonList = _messaggi.map((msg) => msg.toJson()).toList();
-    final jsonString = jsonEncode(jsonList);
+  Future<void> addMessage(types.Message message) async{
+    _messaggi.insert(0, message);
+    final jsonString = jsonEncode(_messaggi);
+    final file = File(await _getFilePath());
     await file.writeAsString(jsonString);
 
-    sendMessage(jsonEncode(message));
+
+    //final response = await http.post(
+    //Uri.parse("http://10.1.0.5:3000/submit"),
+    //headers: {"Content-Type": "application/json"},
+    //body: jsonEncode(message),);
+
+    setState(() {
+      _loadMessages();
+    });
+  }
+  Future<void> prepMsg(types.Message message) async{
+    _messaggi.insert(0, message);
+    final jsonString = jsonEncode(_messaggi);
+    final file = File(await _getFilePath());
+    await file.writeAsString(jsonString);
+    final jsonString2 = jsonDecode(jsonString)[0];
+
+    toSend = jsonEncode(jsonString2);
+  }
+  Future<void> sendMessage(String message) async{
+    socket.emit("sendMessage", message);
   }
 
-  Future<String> getFilePath() async {
-    final directory = await getApplicationDocumentsDirectory();
-    return '${directory.path}/welcome_message.json';
-  }
+ 
 
-  void sendMessage(String jsonString) {
-    socket.emit('sendMessage', jsonString);
-  }
 }
